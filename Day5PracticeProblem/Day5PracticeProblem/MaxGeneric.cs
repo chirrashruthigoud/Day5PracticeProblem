809140b03f5dbc04d3788c414aaf11a5ee53b9ea
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-05-11 21:49:13 +0530

    Add : Find the maximum

---
+++ Day5PracticeProblem/Day5PracticeProblem/MaxGeneric.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5PracticeProblem
+{
+    public class MaxGeneric<T> where T : IComparable<T>
+    {
+        public void FindMax(T fValue, T sValue, T tValue)
+        {
+            if (sValue.CompareTo(fValue) > 0 && sValue.CompareTo(tValue) > 0)
+            {
+                Console.WriteLine($"Second is gretaer : {sValue}");
+            }
+            else if (tValue.CompareTo(fValue) > 0 && tValue.CompareTo(sValue) > 0)
+            {
+                Console.WriteLine($"Third is gretaer : {tValue}");
+            }
+            else
+            {
+                Console.WriteLine(fValue);
+            }
+            
+         }
+    }
+}
---
+++ Day5PracticeProblem/Day5PracticeProblem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5PracticeProblem
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            MaxGeneric<int> generic = new MaxGeneric<int>();
+            generic.FindMax(90,48,85);
+            generic.FindMax(600, 800, 600);
+            generic.FindMax(40, 600, 30);
+            Console.ReadLine();
+        }
+    }
+}
