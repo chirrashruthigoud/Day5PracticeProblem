054e6fabb4b5fa53ee22b2aa02614900e8c74fb2
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-05-15 16:35:47 +0530

    Add : Find minimum

---
+++ Day5PracticeProblem/Day5PracticeProblem/MinGeneric.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5PracticeProblem
+{
+    public class MinGeneric
+    {
+        public void MinFind(int fValue, int secondValue, int thirdValue)
+        {
+            if (fValue < secondValue && fValue < thirdValue)
+            {
+                Console.WriteLine($"First Value is smaller : {fValue}");
+            }
+            else if (secondValue < fValue && secondValue < thirdValue)
+            {
+                Console.WriteLine($"Second Value is smaller : {secondValue}");
+            }
+            else
+            {
+                Console.WriteLine($"Third value is smaller : {thirdValue}");
+            }
+        }
+
+        //public class FindMinimum
+        public void MinFindDouble(double fValue, double sValue, double tValue)
+        {
+            if (fValue < sValue && fValue < tValue)
+            {
+                Console.WriteLine($"First Value is smaller : {fValue}");
+            }
+            else if (sValue < fValue && sValue < tValue)
+            {
+                Console.WriteLine($"Second Value is smaller : {sValue}");
+            }
+            else
+            {
+                Console.WriteLine($"Third value is smaller : {tValue}");
+            }
+        }
+    }
+}
--- Day5PracticeProblem/Day5PracticeProblem/Program.cs
+++ Day5PracticeProblem/Day5PracticeProblem/Program.cs
@@ -10,19 +10,22 @@ namespace Day5PracticeProblem
     {
         public static void Main(string[] args)
         {
-            MaxGeneric<int> generic = new MaxGeneric<int>();
-            MaxGeneric<double> findMax = new MaxGeneric<double>();
-            MaxGeneric<string> name = new MaxGeneric<string>();
-            generic.MaxFind(20, 300, 50);
-            findMax.MaxFind(31.08, 201.52, 64.311);
-            name.MaxFind("sara", "ram", "vani");
-            generic.MaxFind(2002, 300, 50);
-            findMax.MaxFind(18.08, 45.01, 600.32);
-            name.MaxFind("vini", "kiwi", "anu");
-            generic.MaxFind(2000, 300, 5000);
-            findMax.MaxFind(804.08, 90.00, 51.32);
-            name.MaxFind("sithara", "mamu", "vani");
-            generic.FindMax(20, 30, 50);
+            MinGeneric minGeneric = new MinGeneric();
+            minGeneric.MinFind(30,20,10);
+            //MaxGeneric<int> generic = new MaxGeneric<int>();
+            //MaxGeneric<double> findMax = new MaxGeneric<double>();
+            //MaxGeneric<string> name = new MaxGeneric<string>();
+            //generic.MaxFind(20, 300, 50);
+            //findMax.MaxFind(31.08, 201.52, 64.311);
+            //name.MaxFind("sara", "ram", "vani");
+            //generic.MaxFind(2002, 300, 50);
+            //findMax.MaxFind(18.08, 45.01, 600.32);
+            //name.MaxFind("vini", "kiwi", "anu");
+            //generic.MaxFind(2000, 300, 5000);
+            //findMax.MaxFind(804.08, 90.00, 51.32);
+            //name.MaxFind("sithara", "mamu", "vani");
+            //generic.FindMax(20, 30, 50);
+
             Console.ReadLine();
         }
     }
